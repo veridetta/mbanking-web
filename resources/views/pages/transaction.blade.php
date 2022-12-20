fa69ea40d62f4e7961bd024178449a8625f34c80
Vr <veridetta@gmail.com>
2022-12-20 21:50:58 +0700

    transaksi

--- resources/views/pages/transaction.blade.php
+++ resources/views/pages/transaction.blade.php
@@ -10,7 +10,7 @@
                             <div class="col-8">
                                 <div class="numbers">
                                     <h1 class="text-white bolder ">
-                                        BUAT TRANSAKSI
+                                        FORM TRANSFER
                                     </h1>
                                     <p class="mb-0">
                                         Terus perbanyak transaksi dan rasakan kenyamanan yang terbaik dari kami.
@@ -32,7 +32,7 @@
             <div class="col-lg-7 mb-lg-0 mb-4 col-sm-12">
                 <div class="card z-index-2 h-100">
                     <div class="card-header pb-0 pt-3 bg-transparent">
-                        <h6 class="text-capitalize">Form Transfer</h6>
+                        
                         <p class="text-sm mb-0">
                             <i class="fa fa-history text-success"></i>
                             <span class="font-weight-bold">Silahkan lengkapi sebelum melakukan transfer</span>
